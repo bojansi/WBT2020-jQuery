5de6079df2966f3cda3cda2cfddfa4821d67c476
Milan Tepic <milan.tepic221@gmail.com>
2021-03-23 00:19:43 +0100

    Odradjeno linkovanje stranica i ostale sitne ispravke

--- js/html-parser.js
+++ js/html-parser.js
@@ -1,47 +1,32 @@
 $(document).ready(function(){
 
-const first = document.querySelector(".first");
-const iframe = document.querySelector(".second");
+  $.each($(".try-example"), function(key, element){
+    var frame = document.querySelector(".second[data-index='"+(key+1)+"']").contentWindow.document;  
+      frame.open();
+      frame.write($(".first[data-index='"+(key+1)+"']").val());
+      frame.close();
+    $(element).click(function(){
+      var frame = document.querySelector(".second[data-index='"+(key+1)+"']").contentWindow.document;
+      frame.open();
+      frame.writeln($(".first[data-index='"+(key+1)+"']").val());
+      frame.close();
+    });  
+  });
 
+  $(".first").keydown(function(e){
+    if (e.keyCode === 9) {
+      e.preventDefault(); 
+      document.execCommand('insertHTML', false, '&#009');
+    }
+  });
 
-
-// $.each($(".first[data-index]"), function(key, element){
-//   var frame = document.querySelector(".second[data-index='"+(key+1)+"']").contentWindow.document;
-//   console.log((key+1))
-//     frame.open();
-//     frame.write($(".first[data-index='"+(key+1)+"']").val());
-//     frame.close();
-// });
-
-$.each($(".try-example"), function(key, element){
-  var frame = document.querySelector(".second[data-index='"+(key+1)+"']").contentWindow.document;
-  console.log((key+1))
-    frame.open();
-    frame.write($(".first[data-index='"+(key+1)+"']").val());
-    frame.close();
-  $(element).click(function(){
-    var frame = document.querySelector(".second[data-index='"+(key+1)+"']").contentWindow.document;
-    frame.open();
-    frame.writeln($(".first[data-index='"+(key+1)+"']").val());
-    frame.close();
-  });  
-});
-
-$(".first").keydown(function(e){
-  if (e.keyCode === 9) {
-    e.preventDefault(); 
-    document.execCommand('insertHTML', false, '&#009');
-  }
-});
-
-first.addEventListener("paste", function(e) {
-    e.preventDefault();
-    var text = e.clipboardData.getData("text/plain");
-    document.execCommand("insertText", false, text);
-}); 
+  $(".first").on("paste", function(e) {
+      e.preventDefault();
+      var text = e.clipboardData.getData("text/plain");
+      document.execCommand("insertText", false, text);
+  }); 
 
 });
-
 
 function highlightTags(word) {
   var rgxp = new RegExp(word, 'g');
