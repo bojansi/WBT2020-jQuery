5dab07348b8f4d68a60612f54ef6e69e813b845b
Milan Tepic <milan.tepic221@gmail.com>
2021-04-21 03:35:28 +0200

    Optimizacija za mobilne uredjaje

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,35 +1,47 @@
-$(document).ready(function(){
-  
-  $(".content[data-content=0]").animate({width:'toggle'}, 500);
+$(document).ready(function () {
+	$('.content[data-content=0]').animate({ width: 'toggle' }, 500);
 
-  $.each($(".dropdown-btn"), function (key, element) {
-    $.each($(element).next("div").children("ul").children(), function(key2, element2){
-      element2.addEventListener("click", function(){
-        $(".content").hide();
-        $(".content[data-content=" + (key2+1) + "]").fadeToggle(400);
-      });          
-    });
-  });
+	$.each($('.dropdown-btn'), function (key, element) {
+		$.each(
+			$(element).next('div').children('ul').children(),
+			function (key2, element2) {
+				element2.addEventListener('click', function () {
+					$('.content').hide();
+					$('.sidenav').slideToggle(500);
+					$('.content[data-content=' + (key2 + 1) + ']').fadeToggle(400);
+				});
+			}
+		);
+	});
 
-  $.each($(".dropdown-btn"), function (key, element) {
-    element.addEventListener("click", function () {
-      if($(".content[data-content=0]").attr("display")=="none"){
-        $(".content").hide();
-        $(".content[data-content=0]").animate({width:'toggle'}, 500);  
-      }
-      $(element).animate({
-        fontSize: $(element).css('font-size') == '24px' ? "30px" : '24px'
-      },{
-        duration: 500
-      });
-      $(element).siblings("p").animate({
-        fontSize: '24px'
-      },{
-        duration: 500
-      });
-      $(element).siblings("p").next("div").hide(300);
-      $(element).next("div").first().slideToggle(500);        
-    });
-  });
+	$.each($('.dropdown-btn'), function (key, element) {
+		element.addEventListener('click', function () {
+			if ($('.content[data-content=0]').attr('display') == 'none') {
+				$('.content').hide();
+				$('.content[data-content=0]').animate({ width: 'toggle' }, 500);
+			}
+			$(element).animate(
+				{
+					fontSize: $(element).css('font-size') == '24px' ? '30px' : '24px',
+				},
+				{
+					duration: 500,
+				}
+			);
+			$(element).siblings('p').animate(
+				{
+					fontSize: '24px',
+				},
+				{
+					duration: 500,
+				}
+			);
+			$(element).siblings('p').next('div').hide(300);
+			$(element).next('div').first().slideToggle(500);
+		});
+	});
 
+	$('.navbar-toggler2').click(function () {
+		$('.sidenav').slideToggle(500);
+	});
 });
